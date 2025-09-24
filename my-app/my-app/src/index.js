25e1c141dea7e679a5ecd02319f2e2c8e44a83c0
Veselin Velkov <vesselin60@gmail.com>
2025-09-24 17:14:11 +0300

    create-react-app once again because enoent error

--- my-app/my-app/src/index.js
+++ my-app/my-app/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const elem = <h2>'Hello World'</h2>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  elem
 );
 
 // If you want to start measuring performance in your app, pass a function
