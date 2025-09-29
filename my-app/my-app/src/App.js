a0e5857d0dfb53e255a471807aa09bbca2b36a5c
Veselin Velkov <vesselin60@gmail.com>
2025-09-29 17:02:30 +0300

    122.0 Elements and Components, Part 2

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,27 +1,56 @@
+import { Component } from "react";
 import "./App.css";
 
 const Header = () => {
 	return <h2>"Hello Header"</h2>;
 };
 
-const Field = () => {
-	return <input id="1" placeholder="Type here" type="text" />;
-};
+// const Field = () => {
+// 	const holder = "Goch, type here";
+// 	const styledField = {
+// 		width: "300px",
+// 	};
+// 	return (
+// 		<input
+// 			placeholder={holder}
+// 			type="text"
+// 			style={styledField}
+// 			id="123456789"
+// 		/>
+// 	);
+// };
+
+class Field extends Component {
+	render() {
+		const holder = "Goch, type here";
+		const styledField = {
+			width: "300px",
+		};
+		return (
+			<input
+				placeholder={holder}
+				type="text"
+				style={styledField}
+				id="123456789"
+			/>
+		);
+	}
+}
 
 function Btn() {
-  // const text = "Log in"
-  // const res = () => {
-  //   return "Log in"
-  // }
-  const pr = <p>Log Goch</p>
+	// const text = "Log in"
+	// const res = () => {
+	//   return "Log in"
+	// }
+	const pr = <p>Log Goch</p>;
 	return <button>{pr}</button>;
 }
 
 function Bttn() {
-  const text = "Log in"
-  const logged = false
-  
-  return <button>{logged ? 'Enter' : text}</button>
+	const text = "Log in";
+	const logged = false;
+
+	return <button>{logged ? "Enter" : text}</button>;
 }
 
 function App() {
@@ -30,9 +59,10 @@ function App() {
 			<Header />
 			<Field />
 			<Btn />
-      <Bttn />
+			<Bttn />
 		</div>
 	);
 }
 
+export { Header };
 export default App;
