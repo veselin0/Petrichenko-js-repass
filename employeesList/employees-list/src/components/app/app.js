cc1c5563b3cdcdadd8fac1097f300c544027b5b0
Veselin Velkov <vesselin60@gmail.com>
2025-10-02 15:23:14 +0300

    new app Part 3

--- employeesList/employees-list/src/components/app/app.js
+++ employeesList/employees-list/src/components/app/app.js
@@ -1,6 +1,8 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import "./app.css";
 
 function App() {
@@ -8,11 +10,13 @@ function App() {
 		<div className="app">
 			<AppInfo />
 
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
-            </div>
-            
+			<div className="search-panel">
+				<SearchPanel />
+				<AppFilter />
+			</div>
+
+			<EmployeesList />
+			<EmployeesAddForm />
 		</div>
 	);
 }
